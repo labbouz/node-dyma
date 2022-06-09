332b9ff4fc6316d35718bf3888ae36d416a91012
LABBOUZ Abdelmonam <labbouz@gmail.com>
2022-06-09 20:57:35 +0000

    HTMLInputElement

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-// import "./app/app.js";
-
-const p = document.querySelector("p");
-const image = document.querySelector("image");
-const section = document.querySelector("section");
-const a = document.querySelector("a");
 const input = document.querySelector("input");
 
-input.focus();
-
 setTimeout(() => {
-    input.disabled = true;
+  // On sélectionne l'input :
+  input.focus();
+  // On défini son attribut value à 12, sa propriété value
+  // sur le DOM est mise à jour automatiquement.
+  input.setAttribute("value", 12);
+  // On peut donc accéder à la propriété value du DOM :
+  if (input.value !== 42) {
+    // On décide que le champ n'est pas valide et on défini notre message d'erreur :
+    input.setCustomValidity("42 est la seule réponse possible !");
+    // On décide d'afficher dès maintenant le message d'erreur à l'utilisateur :
+    input.reportValidity();
+    // On sélectionne le texte pour qu'il puisse le remplacer
+    input.select();
+  }
 }, 2000);
