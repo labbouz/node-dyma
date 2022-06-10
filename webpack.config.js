2926ae590b4afa6f93b0fe56b90fb8eb788d8cbc
LABBOUZ Abdelmonam <labbouz@gmail.com>
2022-06-10 06:57:32 +0000

    install style-laoder

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,38 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.js"),
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src/index.html"),
-        }),
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: {
+          loader: "css-loader",
+        },
+      },
     ],
-    devtool: "source-map",
-    mode: "development",
-    devServer: {
-        static: path.resolve(__dirname, "./dist"),
-        open: true,
-        port: 4000,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    open: true,
+    port: 4000,
+  },
 };
