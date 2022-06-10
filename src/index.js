d70b88ea41740ce51d44058d537092f1445aa07a
LABBOUZ Abdelmonam <labbouz@gmail.com>
2022-06-10 06:46:41 +0000

    install css-loader

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-const input = document.querySelector("input");
-
-setTimeout(() => {
-  // On sélectionne l'input :
-  input.focus();
-  // On défini son attribut value à 12, sa propriété value
-  // sur le DOM est mise à jour automatiquement.
-  input.setAttribute("value", 12);
-  // On peut donc accéder à la propriété value du DOM :
-  if (input.value !== 42) {
-    // On décide que le champ n'est pas valide et on défini notre message d'erreur :
-    input.setCustomValidity("42 est la seule réponse possible !");
-    // On décide d'afficher dès maintenant le message d'erreur à l'utilisateur :
-    input.reportValidity();
-    // On sélectionne le texte pour qu'il puisse le remplacer
-    input.select();
-  }
-}, 2000);
+import "./style.css";
