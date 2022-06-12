bc57afc1c5572ec4248bb38d43624b9171235d22
LABBOUZ Abdelmonam <labbouz@gmail.com>
2022-06-12 15:14:36 +0000

    presentation todo 13.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import "./style.css";
-
-const square = document.querySelector("div");
-
-square.onmouseover = () => {
-  square.style.backgroundColor = "#f00";
-};
-
-square.onmouseout = () => {
-  square.style.backgroundColor = "#eee";
-};
