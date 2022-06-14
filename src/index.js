dcf57af6572f0334b57f635b48df7979f1292d0d
LABBOUZ <labbouz@gmail.com>
2022-06-14 18:34:45 +0200

    chaine promesse

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import "./style.css";
-import "./loader.css";
 
-function afficherLeResultat(promesse, resultat, tenue) {
-    const div = document.createElement("div");
-    div.innerText = `La promesse ${promesse} retourne ${resultat}`;
-    div.classList.add(tenue ? "succes" : "echec");
-    document.querySelector("#app").append(div);
-}
-
-function toggleLoader() {
-    const loader = document.querySelector(".loader");
-    loader.style.display = loader.display === "none" ? "" : "none";
-}
-
-// Vous pouvez changer resolve par reject pour essayer :
-const p1 = new Promise((resolve, reject) => setTimeout(() => resolve(22), 3000));
-
-p1.finally(toggleLoader);
-
-p1.then((resultat) => {
-    afficherLeResultat("p1", resultat, true);
-});
-
-p1.catch((resultat) => {
-    afficherLeResultat("p1", resultat, false);
-});
+new Promise((resolve) => {
+    resolve(4);
+})
+    .then((res) => res.une.prop.qui.nexiste.pas)
+    .catch((err) => console.error(err.message))
+    .catch((err) => console.error(err.message));
