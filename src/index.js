087e6e3a810f6436aab171e5120b39a6fccc46df
LABBOUZ Abdelmonam <labbouz@gmail.com>
2022-06-15 06:58:05 +0000

    test 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import "./style.css";
 
+function afficherLeResultat(promesse, resultat, tenue) {
+  const div = document.createElement("div");
+  div.innerText = `La promesse ${promesse} retourne ${resultat}`;
+  div.classList.add(tenue ? "succes" : "echec");
+  document.querySelector("#app").append(div);
+}
+
+function toggleLoader() {
+  const loader = document.querySelector(".loader");
+  loader.style.display = loader.display === "none" ? "" : "none";
+}
+
 new Promise((resolve) => {
-    resolve(4);
+  setTimeout(() => resolve(22), 3000);
 })
-    .then((res) => res.une.prop.qui.nexiste.pas)
-    .catch((err) => console.error(err.message))
-    .catch((err) => console.error(err.message));
+  .then(
+    (res) =>
+      new Promise((resolve, reject) => {
+        setTimeout(() => reject(res * 2), 3000);
+      })
+  )
+  .then((res) => afficherLeResultat("then", res, true))
+  .catch((err) => afficherLeResultat("catch", err, false));
